74142d9caebc8ceb42852e9ebfd7f7550e25ad41
freediver <freediver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-20 21:27:32 +0000

    git-svn-id: http://svn.wp-plugins.org/seo-image@47091 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/seo-friendly-images.php
+++ trunk/seo-friendly-images.php
@@ -1,13 +1,22 @@
-<?php /*
+<?php 
+
+/*
 Plugin Name: SEO Friendly Images
 Plugin URI: http://www.prelovac.com/vladimir/wordpress-plugins/seo-image
 Description: Automatically adds alt and title attributes to all your images. Improves traffic from search results and makes them W3C/xHTML valid as well.
-Version: 1.11
+Version: 2.0
 Author: Vladimir Prelovac
 Author URI: http://www.prelovac.com/vladimir
 
-Copyright 2008  Vladimir Prelovac */
+To-Do: 
+- localization
 
+
+Copyright 2008  Vladimir Prelovac 
+
+*/
+
+$seo_friendly_images_localversion="2.0"; 
  
 function seo_friendly_images_add_pages()
 {
@@ -19,6 +28,20 @@ function seo_friendly_images_add_pages()
 // Options Page
 function seo_friendly_images_options_page()
 { 
+
+	global $seo_friendly_images_localversion;
+	
+	$status=seo_friendly_images_getinfo();
+			
+	$theVersion = $status[1];
+	$theMessage = $status[3];	
+	
+	if( (version_compare(strval($theVersion), strval($seo_friendly_images_localversion), '>') == 1) )
+	{
+		$msg = 'Latest version available '.' <strong>'.$theVersion.'</strong><br />'.$theMessage;	
+		  _e('<div id="message" class="updated fade"><p>' . $msg . '</p></div>');			
+	}
+	
     // If form was submitted
 	if (isset($_POST['seo_friendly_images_update'])) 
 	{			
@@ -48,6 +71,10 @@ function seo_friendly_images_options_page()
 		if(version_compare($wp_version,"2.5",">=")) {
 		_e('
 			<style type="text/css">
+			.wrap {
+			max-width:1000px !important;
+			}
+
 			div#moremeta {
 				float:right;
 				width:220px;
@@ -78,24 +105,23 @@ function seo_friendly_images_options_page()
     _e('
  <div class="wrap" id="options-div">
  <form name="form_seo_friendly_images" method="post" action="' . $_SERVER['REQUEST_URI'] . '">
- <h2>SEO Friendly Images</h2>
- <div id="poststuff">
+ <h2>SEO Friendly Images '.$seo_friendly_images_localversion.'</h2>
+<div id="poststuff">
  <div id="moremeta"> 
  <div id="sidebarBlocks" class="dbx-group">
- <fieldset id="about" class="dbx-box">
+  <fieldset id="about" class="dbx-box">
  <h3 class="dbx-handle">Information</h3>
- <div class="dbx-content">
-Worth a cup of coffee for the author? Donate via Paypal.
+ <div id="dbx-content">
+ <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-friendly-images/home.png"><a href="http://www.prelovac.com/vladimir/wordpress-plugins/seo-image"> SEO Friendly Images Home</a><br /><br />
+ <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-friendly-images/idea.png"><a href="http://www.prelovac.com/vladimir/wordpress-plugins/seo-image#comments"> Suggest a Feature</a><br /><br />
+ <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-friendly-images/more.png"><a href="http://www.prelovac.com/vladimir/wordpress-plugins"> My WordPress Plugins</a><br /><br />
  <br />
- <br />
- <a align=center href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=vprelovac%40multimagic%2ecom&item_name=Wordpress%20Plugin%3a%20SEO%20Image&no_shipping=1&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8">
- <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-image/cafe.gif" alt="Make a Donation!" /> Paypal</a>      
- <br /><br />
- <br /><br />
- <a href="http://www.prelovac.com/vladimir/wordpress-plugins/seo-image">SEO Friendly Images Homepage</a><br /><br />
- <a href="http://www.prelovac.com/vladimir/wordpress-plugins">More fine Wordpress plugins and themes</a>
+
+ <p align="center">
+ <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-friendly-images/p1.png"></p>
+
+<p> <img src="'. trailingslashit(get_option('siteurl')). 'wp-content/plugins/seo-friendly-images/help.png"><a href="http://www.prelovac.com/vladimir/services"> Need a WordPress Expert?</a></p>
  </div>
- </fieldset>
  </div>
  </div>
 
@@ -110,14 +136,14 @@ Worth a cup of coffee for the author? Donate via Paypal.
  
  <div class="dbx-c-ontent-wrapper">
  <div class="dbx-content">
-                           
-<p>Enter options below. You can enter any text in the field including two special tags:</p>
+ <p>SEO Friendly Images automatically adds alt and title attributes to all your images in all your posts specified by parameters below.</p>                         
+<p>You can enter any text in the field including two special tags:</p>
 <ul>
-<li>%title - replaced with post title</li>
-<li>%name - replaced with image file name (without extension)</li>
+<li>%title - replaces post title</li>
+<li>%name - replaces image file name (without extension)</li>
+<li>%category - replaces post category</li>
 </ul>
 
-			
 
 
 <p><strong>SEO Friendly Images options</strong></p>
@@ -125,44 +151,46 @@ Worth a cup of coffee for the author? Donate via Paypal.
 
 <div>
 <label for="alt_text"><b>ALT</b> attribute (example: %name %title)</label><br>
-<input id="alt_text" name="alttext" value="' .$alt_text.'"/>
+<input style="border:1px solid #D1D1D1; width:165px;"  id="alt_text" name="alttext" value="' .$alt_text.'"/>
 </div><br>
 
 <div>
 <label for="title_text"><b>TITLE</b> attribute (example: %name photo)</label><br>
-<input id="title_text" name="titletext" value="' .$title_text.'"/>
+<input style="border:1px solid #D1D1D1;  width:165px;"  id="title_text" name="titletext" value="' .$title_text.'"/>
 </div>
 
-
+<br />
 <div><input id="check1" type="checkbox" name="override"' .($override=="on"?'CHECKED':'').'/>
 <label for="check1">Override default Wordpress alt (recommended)</label></div> 
 
 
 
-<i>
-<p>Example:<br />
-In a post titled Car Pictures there is a picture named Ferrari.jpg<br />
-Setting alt attribute to "<b>%name %title</b>" will produce <b>alt="Ferrari Car Pictures"</b><br />
-Setting title attribute to "<b>%name photo</b>" will produce <b>title="Ferrari photo"</b></p>
-</i>
+
+<br/><br /><p>Example:<br />
+In a post titled Car Pictures there is a picture named Ferrari.jpg<br /><br />
+Setting alt attribute to "%name %title" will produce alt="Ferrari Car Pictures"<br />
+Setting title attribute to "%name photo" will produce title="Ferrari photo"</p>
+
 
 <p class="submit">
 	<input type="submit" name="seo_friendly_images_update" value="Update Options &raquo;" />
 </p>
 
+</form>
 
 
+
+		 
 
 </div>
 </div>
 
 </fieldset>
-</div>
-</div>
-</div>
 
 </div>
-</form>
+</div>
+</div>
+<h4>plugin by <a href="http://www.prelovac.com/vladimir/">Vladimir Prelovac</a></h4>
 </div>
 
 ');
@@ -190,6 +218,7 @@ function seo_friendly_images_process($matches) {
 					
 		
 		
+//		echo $post->post_title;
 		### Normalize spacing around attributes.
 		$matches[0] = preg_replace('/\s*=\s*/', '=', substr($matches[0],0,strlen($matches[0])-2));
 		### Get source.
@@ -202,47 +231,51 @@ function seo_friendly_images_process($matches) {
 		$pieces = preg_split('/(\w+=)/', $matches[0], -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
 		### Add missing pieces.
 		
-		
+		$cats=get_the_category();
 		if (!in_array('title=', $pieces)) {
 			$titletext_rep=str_replace("%title", $post->post_title, $titletext_rep);
 			$titletext_rep=str_replace("%name", $source[0], $titletext_rep);
+			$titletext_rep=str_replace("%category", $cats[0]->slug, $titletext_rep);
 			
 		
 			$titletext_rep=str_replace('"', '', $titletext_rep);
 			$titletext_rep=str_replace("'", "", $titletext_rep);
 			
 		
-			array_push($pieces, 'title="' . $titletext_rep . '"');
+			array_push($pieces, ' title="' . $titletext_rep . '"');
 		}
 		if (!in_array('alt=', $pieces) ) {
 			$alttext_rep=str_replace("%title", $post->post_title, $alttext_rep);
 			$alttext_rep=str_replace("%name", $source[0], $alttext_rep);
+			$alttext_rep=str_replace("%category", $cats[0]->slug, $alttext_rep);
 			
 			$alttext_rep=str_replace("\"", "", $alttext_rep);
 			$alttext_rep=str_replace("'", "", $alttext_rep);
 		
-			array_push($pieces, 'alt="' . $alttext_rep . '"');
+			array_push($pieces, ' alt="' . $alttext_rep . '"');
 		}
 		else
 		{
-		
-			$key=array_search('alt=',$pieces);
-							
-			if (($pieces[$key+1]=='""') || (strpos($saved, str_replace('"','',$pieces[$key+1])) && $override=="on"))
-			{
 			
+			$key=array_search('alt=',$pieces);
+			
+			if ((trim($pieces[$key+1])=='""') || (strpos($saved, str_replace('"','',trim($pieces[$key+1]))) && $override=="on"))
+			{
+				
 				$alttext_rep=str_replace("%title", $post->post_title, $alttext_rep);
 				$alttext_rep=str_replace("%name", $source[0], $alttext_rep);
+				$alttext_rep=str_replace("%category", $cats[0]->slug, $alttext_rep);
 				
 				$alttext_rep=str_replace("\"", "", $alttext_rep);
 				$alttext_rep=str_replace("'", "", $alttext_rep);
 				
-				$pieces[$key+1]='"'.$alttext_rep.'"';
+				$pieces[$key+1]='"'.$alttext_rep.'" ';
 				
 			}
 		}
 	
-		return implode(' ', $pieces).' /';
+	
+		return implode('', $pieces).' /';
 	}
 
 function seo_friendly_images($content) {
@@ -250,6 +283,67 @@ function seo_friendly_images($content) {
 }
 
 
-add_filter('the_content', 'seo_friendly_images');
+add_filter('the_content', 'seo_friendly_images', 50);
+
+add_action( 'after_plugin_row', 'seo_friendly_images_check_plugin_version' );
+
+function seo_friendly_images_getinfo()
+{
+		$checkfile = "http://svn.wp-plugins.org/seo-image/trunk/seo-friendly-images.chk";		
+		
+		$status=array();
+		return $status;
+		$vcheck = wp_remote_fopen($checkfile);
+				
+		if($vcheck)
+		{
+			$version = $seo_friendly_images_localversion;
+									
+			$status = explode('@', $vcheck);
+			return $status;				
+		}					
+}
+
+function seo_friendly_images_check_plugin_version($plugin)
+{
+	global $plugindir, $seo_friendly_images_localversion;
+	
+ 	if( strpos($plugin,'seo-friendly-images.php')!==false )
+ 	{
+			
+
+			$status=seo_friendly_images_getinfo();
+			
+			$theVersion = $status[1];
+			$theMessage = $status[3];	
+	
+			if( (version_compare(strval($theVersion), strval($seo_friendly_images_localversion), '>') == 1) )
+			{
+				$msg = 'Latest version available '.' <strong>'.$theVersion.'</strong><br />'.$theMessage;				
+				echo '<td colspan="5" class="plugin-update" style="line-height:1.2em;">'.$msg.'</td>';
+			} else {
+				return;
+			}
+		
+	}
+}
+
+
+
+function seo_friendly_images_install(){		
+  if(!get_option('seo_friendly_images_alt')){
+    add_option('seo_friendly_images_alt', '%name %title');
+  }
+  if(!get_option('seo_friendly_images_title')){
+    add_option('seo_friendly_images_title', '%title');
+  }
+  if(get_option('seo_friendly_images_override' == '') || !get_option('seo_friendly_images_override')){
+    add_option('seo_friendly_images_override', 'on');
+  }
+  
+}
+
+
+add_action( 'plugins_loaded', 'seo_friendly_images_install' );
 
 ?>
